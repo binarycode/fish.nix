dc0a363a80b8e9a5f11f2fce382df748e285d0bd
Igor Sidorov <igor.sidorov@binarycode.ru>
2025-05-25 12:45:20 +0300

    remove starship stuff

--- config.fish
+++ config.fish
@@ -1,10 +1,3 @@
-# initialize starship if available
-if type -q starship
-  set -gx STARSHIP_CONFIG /etc/starship.toml
-
-  starship init fish | source
-end
-
 # initialize homebrew if available
 if test -e /opt/homebrew/bin/brew
   eval (/opt/homebrew/bin/brew shellenv)
